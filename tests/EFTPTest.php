21aecaaf7db79206eda764ae0194299d3bed983a
bradfordcondon <bradford.condon@gmail.com>
2018-11-29 11:20:55 -0500

    FTP class

---
+++ tests/EFTPTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Tests;
+
+use StatonLab\TripalTestSuite\DBTransaction;
+use StatonLab\TripalTestSuite\TripalTestCase;
+
+class EFTPTest extends TripalTestCase {
+  // Uncomment to auto start and rollback db transactions per test method.
+  // use DBTransaction;
+
+  /**
+   *
+   * @group ftp
+   */
+  public function testFTPFindsProgram() {
+
+    $ftp = new \EFTP();
+$url = 'ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/285/GCF_000002285.3_CanFam3.1/GCF_000002285.3_CanFam3.1_assembly_report.txt';
+    $ftp->setURL($url);
+
+    $result = $ftp->getField('# Assembly method:');
+
+    $this->assertNotEmpty($result);
+    
+  }
+}
