199bd6dfc2c272c33157fe48a95f7c17cfa9047f
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-12-04 15:18:44 -0500

    Genericize EUtilsRequest class

--- tests/BiosamplePropertyLookupTest.php
+++ tests/BiosamplePropertyLookupTest.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace Tests;
 
 use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
 
-class BiosamplePropertyLookupTest extends TripalTestCase {
+class BiosamplePropertyLookupTest extends TripalTestCase{
+
   // Uncomment to auto start and rollback db transactions per test method.
   // use DBTransaction;
 
@@ -12,31 +14,25 @@ class BiosamplePropertyLookupTest extends TripalTestCase {
    * @group propertylookup
    */
   public function testBiosamplePropertyLookup() {
-
-
     $lookup = new \BiosamplePropertyLookup();
-
-    $terms  = $lookup->lookupAll();
-
+    $terms = $lookup->lookupAll();
     $this->assertNotEmpty($terms);
 
-    //Version 1.0 as of November 29 2018: 456 terms.
-
-    $this->assertEquals('456', count($terms));
-
+    // Version 1.0 as of November 29 2018: 456 terms.
+    $this->assertEquals(456, count($terms));
   }
 
   /**
-   * Random example of a term that should have been installed ni hte XML reading.
+   * Random example of a term that should have been installed ni hte XML
+   * reading.
+   *
    * @group terms
    * @group propertylookup
    */
-  public function testTermsInserted(){
-
-   $term = chado_get_cvterm(['id' => 'ncbi_properties:fao_class']);
-   $this->assertObjectHasAttribute('cvterm_id', $term);
+  public function testTermsInserted() {
+    $term = chado_get_cvterm(['id' => 'ncbi_properties:fao_class']);
+    $this->assertObjectHasAttribute('cvterm_id', $term);
     $this->assertObjectHasAttribute('name', $term);
     $this->assertEquals('FAO classification', $term->name);
-
   }
 }
