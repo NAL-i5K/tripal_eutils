96b083a3d5f4cc7ae16ac5613d12e9dee1333069
bradfordcondon <bradford.condon@gmail.com>
2018-12-12 11:29:50 -0500

    add passing test and fix the formatter class

--- tests/EutilsAssemblyFormatterTest.php
+++ tests/EutilsAssemblyFormatterTest.php
@@ -1,29 +1,48 @@
 <?php
+
 namespace Tests;
 
-use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
 
+/**
+ *
+ */
 class EutilsAssemblyFormatterTest extends TripalTestCase {
-  // Uncomment to auto start and rollback db transactions per test method.
-  // use DBTransaction;
 
-  public function testWholeFormat(){
+  // Uncomment to auto start and rollback db transactions per test method.
+  // use DBTransaction;.
+
+  /**
+   * Run through the formatter and check key values match the test assembly XML.
+   *
+   * @group wip
+   */
+  public function testWholeFormat() {
 
     $parsed = $this->parseXML();
-    $elements = [];
     $formatter = new \EutilsAssemblyFormatter();
-    $formatter->format($parsed, $elements);
+    $elements = $formatter->format($parsed);
+
+    $this->assertNotEmpty($elements);
+
+    $this->assertArrayHasKey('base_record', $elements);
+    $this->assertArrayHasKey('properties', $elements);
+
   }
 
+  /**
+   * Provide a mocked parsed Assembly XML file.
+   *
+   * @return array
+   *   parsed XML array.
+   */
   private function parseXML() {
 
+    // Load in a test assembly and mock its FTP method/data.
     $file = __DIR__ . '/../examples/assembly/559011_assembly.xml';
     $parser = $this->getMockBuilder('\EUtilsAssemblyParser')
       ->setMethods(['getFTPData'])
       ->getMock();
-
-    // We mock the FTP call.
     $ftp_response = ['# Assembly method:' => 'a method, v1.0'];
 
     $parser->expects($this->once())
@@ -34,4 +53,5 @@ class EutilsAssemblyFormatterTest extends TripalTestCase {
 
     return $assembly;
   }
+
 }
