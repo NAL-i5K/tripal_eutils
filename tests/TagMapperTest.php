8a7e8f2a04aae47496fef76309cd8566c333c580
bradfordcondon <bradford.condon@gmail.com>
2018-12-12 15:47:06 -0500

    passing tests and resolve issue.  add a test for the display lookup.  start the machine name lookup but i think we want the tagmapper to be smarter than that...

--- tests/TagMapperTest.php
+++ tests/TagMapperTest.php
@@ -5,11 +5,13 @@ namespace Tests;
 use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
 
+/**
+ *
+ */
 class TagMapperTest extends TripalTestCase {
 
   // Uncomment to auto start and rollback db transactions per test method.
   use DBTransaction;
-
 
   /**
    * @group attributes
@@ -18,10 +20,43 @@ class TagMapperTest extends TripalTestCase {
 
     $mapper = new \TagMapper('biosample');
 
-   $term =  $mapper->lookup('sex');
+    $term = $mapper->lookup('sex');
 
-   $this->assertNotNull($term);
+    $this->assertNotNull($term);
   }
 
+  /**
+   *
+   */
+  public function testLabelLooker() {
+
+    $mapper = new \TagMapper('biosample');
+
+    $hname = 'cow';
+    $aname = 'dog';
+    $dname = 'display';
+    $value = 'some_value';
+    $term = [
+      'value' => $value,
+      'harmonized_name' => $hname,
+      'attribute_name' => $aname,
+      'display_name' => $dname,
+    ];
+    $picked = $mapper->getDisplayLabel($term);
+
+    $this->assertEquals($dname, $picked);
+
+    unset($term['display_name']);
+    $picked = $mapper->getDisplayLabel($term);
+
+    $this->assertEquals($aname, $picked);
+
+    unset($term['attribute_name']);
+
+    $picked = $mapper->getDisplayLabel($term);
+
+    $this->assertNotFalse($picked);
+    $this->assertNotEquals($value, $picked);
+  }
 
 }
