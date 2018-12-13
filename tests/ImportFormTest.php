73e3137044b0ac952a40fb1c1b546f74e04379a7
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-12-13 08:21:02 -0500

    Add import form test

---
+++ tests/ImportFormTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests;
+
+use StatonLab\TripalTestSuite\DBTransaction;
+use StatonLab\TripalTestSuite\TripalTestCase;
+
+
+class ImportFormTest extends TripalTestCase {
+  // Uncomment to auto start and rollback db transactions per test method.
+  use DBTransaction;
+
+  /**
+   * @group import_form
+   */
+  public function testAccessibilityToImportForm() {
+    $this->actingAs(1);
+
+    $response = $this->get('admin/tripal/tripal_eutils/import');
+    $response->assertSuccessful()
+    ->assertSee('NCBI Accession Number');
+  }
+
+}
