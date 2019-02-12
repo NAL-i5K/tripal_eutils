54ffd503a4bfb52198a63463a4c3130168ab3709
bradfordcondon <bradford.condon@gmail.com>
2019-02-12 10:32:17 -0500

    simple and incomplete converter

--- tests/EUtilsTest.php
+++ tests/EUtilsTest.php
@@ -16,4 +16,28 @@ class EUtilsTest extends TripalTestCase {
     $connection->get('waffles', ['000000']);
   }
 
+
+  /**
+   * Accessions with letters arent UIDs and should be converted.
+   *
+   * @group eutils
+   * @group convert
+   * @dataProvider accessionDataProvider
+   * @throws \Exception
+   */
+  public function testConvertingAccession($db, $input, $expect) {
+    $connection_master = new \EUtils();
+    $connect = reflect($connection_master);
+    $out = $connect->convertAccessionsToUID($db, $input);
+    $this->assertEquals($expect, $out);
+    sleep(.4);
+  }
+
+  public function accessionDataProvider() {
+
+    return [
+      ['assembly', 'GCF_000184155.1', '557018'],
+      ['assembly', '557018', '557018'],
+    ];
+  }
 }
